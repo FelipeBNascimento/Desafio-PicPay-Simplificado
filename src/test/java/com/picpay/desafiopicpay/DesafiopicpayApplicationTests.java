f0ac240ce974299f5cdb30484f6d58ecc24b5f30
Bazan <felipe_bazan3006@hotmail.com>
2025-11-12 12:51:10 -0300

    Primeiro Commit

---
+++ src/test/java/com/picpay/desafiopicpay/DesafiopicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.desafiopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiopicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
